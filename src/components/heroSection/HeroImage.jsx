f1d8d721705c2c83b68ef10fb0f3863ed9ddda54
Ojesh <ojeshmundale@gmail.com>
2025-06-12 20:31:19 +0530

    updated

--- src/components/heroSection/HeroImage.jsx
+++ src/components/heroSection/HeroImage.jsx
@@ -6,7 +6,7 @@ const HeroImage = () => {
     <div className="relative min-h-[400px] w-full flex items-center justify-center bg-black">
       <div className="relative w-full h-full">
         <img
-          src="/images/me.png"
+          src="images/me.png"
           alt="Ojesh Mundale"
           className="w-auto h-auto md:max-w-[570px] sm:max-w-[380px] absolute bottom-0 left-1/2 -translate-x-1/2 z-50 border-4 border-red-500"
         />
